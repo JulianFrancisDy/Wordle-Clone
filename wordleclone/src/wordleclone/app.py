ccc1b4af82d9bb8c33210a362502cf1e668d0228
Julian <jufiejuf@gmail.com>
2022-02-11 15:37:07 +0800

    updated

--- wordleclone/src/wordleclone/app.py
+++ wordleclone/src/wordleclone/app.py
@@ -1,4 +1,4 @@
-from turtle import color
+from turtle import clear, color
 import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW, CENTER
@@ -7,7 +7,7 @@ class wordleClone(toga.App):
 
     def startup(self):
 
-        self.answer = "testi"
+        self.answer = "czari"
 
         self.attempts = 0
 
@@ -54,6 +54,7 @@ class wordleClone(toga.App):
         restart_button = toga.Button(
             'Restart',
             style = Pack(padding = 10),
+            on_press=self.reset
         )
 
         main_box.add(guess_box)
@@ -66,18 +67,33 @@ class wordleClone(toga.App):
         self.main_window.content = main_box
         self.main_window.show()
 
+    def reset(self, widget):
+        print("RESETTING")
+        self.clear_grid()
+        self.fill_grid()
+        self.attempts = 0
+
     def fill_grid(self):
+        self.rows.clear()
         for i in range(6):
             row = toga.Box()
             for j in range(5):
-                letter = toga.Button("", style=Pack(width = 40, height = 40, padding = 3, background_color = "Gray"))
+                letter = toga.Button("", style=Pack(width = 40, height = 40, padding = 3, background_color = "White"))
                 row.add(letter)
             self.rows.append(row)
             self.grid.add(row)
+        for i in range(6):
+            self.grid.add(self.rows[i])
+
+    def clear_grid(self):
+        for i in range(6):
+            self.grid.remove(self.rows[i])
         
+    def update_grid(self):
+        for i in range(6):
+            self.grid.add(self.rows[i])
 
     def validate_guess(self, widget):
-
         guess_inp = str(self.guess_input.value)
         error_message = ""
 
@@ -90,12 +106,41 @@ class wordleClone(toga.App):
             self.main_window.info_dialog(
                 "Invalid Guess",
                 error_message
+                self.guess_inp.clear
             )
-        
-        self.game_logic()
+        else:
+            self.game_logic()
 
     def game_logic (self):
-        pass
+
+        print(self.attempts)
+
+        self.attempts += 1
+
+        answer = self.answer
+        guess = self.guess_input.value
+
+        print("** FOR TESTING **\nAnswer is: {}\nGuess is: {}\n".format(self.answer, self.guess_input.value))
+
+        new_row = toga.Box()
+
+        self.clear_grid()
+
+        for i in range(5):
+            if guess[i] == answer[i]:
+                letter = toga.Button("", style=Pack(width = 40, height = 40, padding = 3, background_color = "Green"))
+                new_row.add(letter)
+            elif guess[i] in answer:
+                letter = toga.Button("", style=Pack(width = 40, height = 40, padding = 3, background_color = "Yellow"))
+                new_row.add(letter)
+            else:
+                letter = toga.Button("", style=Pack(width = 40, height = 40, padding = 3, background_color = "Gray"))
+                new_row.add(letter)
+
+        self.rows.pop(self.attempts-1)
+        self.rows.insert(self.attempts-1, new_row)
+
+        self.update_grid()
 
 def main():
     return wordleClone()
